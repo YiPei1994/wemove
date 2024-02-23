0cc69e70f63e7f82fa328bb1cc9c3188f0637137
yipei <studentypz@gmail.com>
2024-02-23 16:22:44 +0100

    fix - bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useCurrentUser } from "@/components/auth/useCurrentUser";
 import DailyGoal from "@/components/dailyGoal/DailyGoal";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
   const { isAutenticated } = useCurrentUser();
